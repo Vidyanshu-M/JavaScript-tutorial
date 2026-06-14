eb3df2f4981f00508ef99019795dc06df1714afb
vidyanshumishra-byte <vidyanshumishra99@gmail.com>
2026-06-14 14:24:41 +0530

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // from that it treats all JS code as newer JS version 
+// alert (3+3)  // we are using nodejs, not browser 
+
+console.log("Vidyanshu");
+
+let name = "Vidyanshu"
+let age =20.7   // *here integer and point value both assume as number*
+let work =5
+let isLoggedIn = true
+
+// number => (maxrange for int) 2 to power 53~
+// bigint => this is used to write very large number 
+// string => "" always written in colons
+// boolean => True/False
+// undefined => undefined (it is because variable is declaired but value is not assigned)
+// null => standAlone value 
+// symbol => unique
+
+
+//object 
+
+
+console.log(typeof 20.7);
+console.log(typeof undefined);  //o/p:>  undefinrd
+console.log(typeof null);   //o/p:>  object
+
