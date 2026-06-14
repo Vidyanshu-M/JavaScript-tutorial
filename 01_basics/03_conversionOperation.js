a316addb8ed488f0fe3775339bdde69b405bfd8b
Vidyanshu-M <mishravidyanshu594@gmail.com>
2026-06-14 20:49:15 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+/*
+
+let score1 = 33
+let score2 ="Vidya"
+let score3 =null
+let score4 =undefined
+let score5 ="123abc"
+let score6 = true
+
+
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+console.log(typeof score5);
+console.log(typeof score6);
+
+console.table([score1,score2,score3,score4,score5,score6])
+
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+let valueInNumber6 = Number(score6)
+
+
+console.log(typeof valueInNumber1)
+console.log(typeof valueInNumber2)
+console.log(typeof valueInNumber3)
+console.log(typeof valueInNumber4)
+console.log(typeof valueInNumber5)
+console.log(typeof valueInNumber6)
+
+
+console.table([valueInNumber1,valueInNumber2,valueInNumber3,valueInNumber4,valueInNumber5,valueInNumber6]);
+
+
+
+//"33" => 33 (number)
+//"33abc" => NaN (not a number ,but it's type is number )
+//true => 1; false => 0
+
+*/
+
+
+
+
+let isLogedIn ="Vidyanshu"
+let booleanIsLoggedIn =Boolean(isLogedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=> false;
+//" " => false; 
+//"StringValue" => true 
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
