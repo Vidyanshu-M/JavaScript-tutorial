06edd5439cb443f09e8904765abf008b13dfaebe
vidyanshumishra-byte <vidyanshumishra99@gmail.com>
2026-06-14 14:23:34 +0530

    JS Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 12345
+let accountEmail = "jstutorial@google.com"
+var  accountPassword = "2345"// not prefer to use over let
+accountCity =" Jaipur"// this is the dangerous to use
+let accountState;
+
+
+
+
+accountEmail="vm@vm.com"
+accountPassword="23232323"
+accountCity="Varanasi"
+
+
+console.log(accountId);
+
+
+/*prefer not to use var 
+because of issues in block scope and functional scope 
+*/
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
